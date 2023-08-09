f514e28fd2bfec624d779bea4fccba0046cf5fdd
Zeb Burke-Conte <zmbc@uw.edu>
2023-08-09 10:42:14 -0700

    Remove gc() call in gammaCKpar

--- R/gammaCKpar.R
+++ R/gammaCKpar.R
@@ -119,7 +119,7 @@ gammaCKpar <- function(matAp, matBp, n.cores = NULL, cut.a = 0.92, cut.p = 0.88,
         	}
         
         t@x[t@x >= cut[1]] <- 2
-        t@x[t@x >= cut[2] & t@x < cut[1]] <- 1; gc()
+        t@x[t@x >= cut[2] & t@x < cut[1]] <- 1
         slice.1 <- m[[2]]
         slice.2 <- y[[2]]
         indexes.2 <- which(t == 2, arr.ind = T)
